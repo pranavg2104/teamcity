7be38eed03ea1ca0979a50b73d067980893ddf89
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-01-31 18:34:52 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -7,11 +7,15 @@ status = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys
 startTime = sys.argv[2]
 endTime = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/finishDate",auth = HTTPBasicAuth('admin','admin'))
 print(status.text)
+finishTime = endTime.text[9:15:1]
+duration = finishTime - startTime
+duration = duration[:2] + ':' + duration[2:4] + ':' + duration[4:] 
+
 startTime = startTime[:2] + ':' + startTime[2:4] + ':' + startTime[4:] 
 print(startTime)
-finishTime = endTime.text[9:15:1]
 finishTime = finishTime[:2] + ':' + finishTime[2:4] + ':' + finishTime[4:] 
 print(finishTime)
+print(duration)
 
 
 
