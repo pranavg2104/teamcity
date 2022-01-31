82b3951a166c7ef847daeee466b8c9a8e0ef49f2
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-01-31 17:42:29 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -3,6 +3,10 @@ import json
 from requests.auth import HTTPBasicAuth
 import sys
 
-x = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/status",auth = HTTPBasicAuth('admin','admin'))
-print(x.text)
+status = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/status",auth = HTTPBasicAuth('admin','admin'))
+startTime = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/startDate",auth = HTTPBasicAuth('admin','admin'))
+endTime = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/finishDate",auth = HTTPBasicAuth('admin','admin'))
+print(status.text)
+print(startTime.text)
+print(endTime.text)
 print("Hello")
