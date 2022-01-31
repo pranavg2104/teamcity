0219375b3b1740dff8c4b240e52f502a93db4709
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-01-31 13:10:25 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -2,5 +2,5 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 x = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:Teamcity_Build/status",auth = HTTPBasicAuth('admin','admin'))
-print(x)
+print(x.content)
 print("Hello")
