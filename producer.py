4853a554f16909ee88acd2376d4846d503496dd4
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-01-31 16:38:42 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from requests.auth import HTTPBasicAuth
+import sys
 
 x = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/status",auth = HTTPBasicAuth('admin','admin'))
 print(x.text)
