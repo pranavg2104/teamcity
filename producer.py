32ca58d26ae414a2498980635408cfc6795dfaef
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-01-31 13:18:28 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -3,5 +3,5 @@ import json
 from requests.auth import HTTPBasicAuth
 
 x = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:Teamcity_Build/status",auth = HTTPBasicAuth('admin','admin'))
-print(x.content)
+print(x.headers)
 print("Hello")
