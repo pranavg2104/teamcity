f42192e5cbb768e29296ab961c3f0d07622816d5
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-01-31 12:45:55 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,5 +1,5 @@
 import requests
 
-x = requests.get("http://localhost:8111/app/rest/builds/buildType:Teamcity_Build/status")
-print(x.status_code)
+x = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:Teamcity_Build/status")
+print(x)
 print("Hello")
