61cca3f6dd17314a7aa84085275f8fb6909c0d7d
pranavg2104 <80096043+pranavg2104@users.noreply.github.com>
2022-01-31 17:45:15 +0530

    Update producer.py

--- producer.py
+++ producer.py
@@ -4,9 +4,11 @@ from requests.auth import HTTPBasicAuth
 import sys
 
 status = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/status",auth = HTTPBasicAuth('admin','admin'))
-startTime = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/startDate",auth = HTTPBasicAuth('admin','admin'))
+#startTime = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/startDate",auth = HTTPBasicAuth('admin','admin'))
+startTime = sys.argv[2]
 endTime = requests.get("http://192.168.0.106:8111/app/rest/builds/buildType:"+sys.argv[1]+"/finishDate",auth = HTTPBasicAuth('admin','admin'))
 print(status.text)
-print(startTime.text)
+#print(startTime.text)
+print(startTime)
 print(endTime.text)
 print("Hello")
